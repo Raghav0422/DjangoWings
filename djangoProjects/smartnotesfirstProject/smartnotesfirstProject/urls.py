ddfd6e4258b2bf416e10f8d54699673400e53292
Raghav0422 <60313641+Raghav0422@users.noreply.github.com>
2024-05-12 09:54:03 +0100

    Update urls.py

--- djangoProjects/smartnotesfirstProject/smartnotesfirstProject/urls.py
+++ djangoProjects/smartnotesfirstProject/smartnotesfirstProject/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from firstApp import display
+from firstApp import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
